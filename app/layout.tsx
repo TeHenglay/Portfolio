07b687eb37031356fa471e532fb2d54b8e1f1da8
TeHenglay <tehenglay098@gmail.com>
2025-02-12 16:07:34 +0700

    add portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,98 @@
+import "./globals.css";
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Syne } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
+const syne = Syne({
+  subsets: ["latin"],
+  display: "block",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "TE HENGLAY — Frontend Engineer",
+  description:
+    "Frontend Engineer and Web Designer, currently study at Royal University of Phnom Phen. Focused on interfaces and experiences, working remotely from Kandal, Cambodia .",
+  generator: "Next.js",
+  applicationName: "TE HENGLAY",
+  keywords: [
+    "freelance",
+    "developer",
+    "freelance developer",
+    "frontend",
+    "react",
+    "frontend developer",
+    "frontend engineer",
+    "creative",
+    "creative developer",
+    "creative engineer",
+    "tech",
+    "nigeria",
+    "software",
+    "software developer",
+    "portfolio",
+    "frontend developer portfolio",
+    "creative developer portfolio",
+  ],
+  colorScheme: "dark",
+  openGraph: {
+    title: "TEHENGLAY — Frontend Engineer",
+    description:
+      "Frontend Engineer and Web Designer, currently study at Royal University of Phnom Phen. Focused on interfaces and experiences, working remotely from Kandal, Cambodia .",
+    url: "https://laszlo.icu/",
+    siteName: "www.laszlo.icu",
+    images: [
+      {
+        url: "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
+        width: 1200,
+        height: 630,
+        alt: "TE HENGLAY — Frontend Engineer",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TE HENGLAY — Frontend Engineer",
+    description:
+      "Frontend Engineer and Web Designer, currently at RUPP. Focused on interfaces and experiences, working remotely from Phnom Phen, Cambodia.",
+    creator: "victorwill__",
+    creatorId: "1243720976552144897",
+    images: [
+      "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
