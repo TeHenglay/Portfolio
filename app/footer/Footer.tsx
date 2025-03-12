33d8a65347c4cbc05805847de5edd5f35a65a80c
Henglay <tehenglay098@gmail.com>
2025-03-12 09:48:38 +0700

    Update Footer.tsx

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
             className={"m-0 p-0"}
           />
           <Link
-            href="https://github.com/YavYav"
+            href="https://github.com/TeHenglay"
             target="_blank"
             aria-label="Victor's GitHub Profile"
           >
